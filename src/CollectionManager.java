1f6d56e2e4ac750b7df87313efea159bc8570d7d
ge-naks <georgewnakhla@gmail.com>
2024-02-02 17:51:24 -0500

    added collection manager class

---
+++ src/CollectionManager.java
@@ -0,0 +1,11 @@
+/**
+ * This class handles user interaction by reading and processing command lines from the terminal.
+ * It should contain methods to process each command, manage the collection of albums, and display
+ * results.
+ * @author George Nakhla
+ * @ param collection the collection to be controlled by commands
+ *
+ */
+public class CollectionManager {
+
+}
