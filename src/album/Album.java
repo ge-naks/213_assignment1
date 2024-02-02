848f856d602a0c2ba3aac842d8db75fced8c7753
myoussef2435 <112906466+myoussef2435@users.noreply.github.com>
2024-02-02 18:35:16 -0500

    update

--- src/album/Album.java
+++ src/album/Album.java
@@ -7,11 +7,12 @@ public class Album {
     private Date released;
     private Rating ratings; //a linked list of ratings
 
-    public void rate(int star) { } //add a rating to the linked list
-    if (ratings == null) {
-        ratings = new Rating(star);
-    } else {
-        ratings.addRating(star);
+    public void rate(int star) {  //add a rating to the linked list
     }
-    public double avgRatings() { } //compute the average ratings
+
+    public double avgRatings(int[] ratings) {
+        //compute the average ratings
+        int double average;
+        return average;
+    }
 }
--- src/album/DateConstant.java
+++
@@ -1,4 +0,0 @@
-package album;
-
-public class DateConstant {
-}
